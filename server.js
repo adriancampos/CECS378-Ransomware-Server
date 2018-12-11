409b3b366acb434e235ea7f34c6fcd72ffc198b7
Adrian Campos <adriancampos@users.noreply.github.com>
2018-12-11 15:24:47 -0800

    Added app-key verification middleware

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+APP_KEY = 'nhkYJUeKiux2ve3RcO1ObbNRJcAxnl0VSfGSIqk2VO70lU7N2iznXvkSw4TXsAFL'
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
@@ -13,6 +15,16 @@ mongoose.connect('mongodb://localhost/Keypairdb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+var validateAppKey = function (req, res, next) {
+  if (req.headers['app-key'] == APP_KEY) {
+    next()
+  } else {
+    res.send("Error: Invalid app key!")
+  }
+}
+// Require app_key in the headers to match the APP_KEY constant
+app.use(validateAppKey)
 
 
 var routes = require('./api/routes/keypairListRoutes'); //importing route
